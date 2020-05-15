242586d53cbe9285e75746052d019cc601b890db
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2020-05-15 12:00:32 +0200

    Simple example
    Signed-off-by: Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>

---
+++ simple-hello/src/main/java/com/demo/simple/ApplicationConfig.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 The Eclipse Foundation and others.
+ * 
+ * This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution, and is available at https://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package com.demo.simple;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/**
+ * Configures JAX-RS for the application.
+ */
+@ApplicationPath("")
+public class ApplicationConfig extends Application {
+    
+}
